e91991e0257fff8d497829393eba1d8bfb09542a
pietdaniel <dpiet@salsify.com>
2015-05-25 16:42:57 -0400

    trying to get running on mac

--- README.md
+++ README.md
@@ -6,12 +6,13 @@ Getting this to go run is easy:<br>
 <br>
 `apt-get install golang libglew1.6-dev pkg-config libsdl1.2-dev libsdl-image1.2-dev libsdl-mixer1.2-dev`<br>
 `go get github.com/banthar/Go-SDL/sdl`<br>
-`go get github.com/go-gl/gl`<br>
+`go get github.com/go-gl/gl/v2.1/gl`<br>
 `go run main.go`<br>
 
+![](ss.png)
 
-TODO:<br>
-Recursive Backtracker
-Hunt and Kill
-Growing Tree
+
+====
+
+note: does not run well on macosx atm
 
--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"container/list"
-	"math/rand"
-	"time"
 	//"fmt"
 	"github.com/banthar/Go-SDL/sdl"
-  "github.com/go-gl/gl"
+	"github.com/go-gl/gl/v2.1/gl"
+	"math/rand"
+	"time"
 )
 
 var view_rotx float64 = 0.0
@@ -14,55 +14,56 @@ var view_roty float64 = 90.0
 var view_rotz float64 = 0.0
 var view_z float64 = 0
 var angle float64 = 0.0
-var boxes []uint // for the drawing of walls FIXME should not be global imo
-var width, height int = 40,40
+var boxes []uint32 // for the drawing of walls FIXME should not be global imo
+var width, height int32 = 20, 20
 
-var DEBUG bool =  false
+var DEBUG bool = false
 
 //
 // 3 x 3 cells
 // 2 x 3 vert walls
 // 2 x 3 horzontal walls
 //       1,1     1,2
-//  [1,1] | [1,2] | [1,3] 
+//  [1,1] | [1,2] | [1,3]
 //   ---     ---     ---   1,1 1,2 1,3 H
 //  [2,1] | [2,2] | [2,3]                { 2,1  2,2 V }
 //   ---     ---     ---   2,1 2,2 2,3 H
-//  [3,1] | [3,2] | [3,3] 
+//  [3,1] | [3,2] | [3,3]
 //       3,1     3,2  V
 //
 
 // an array of cells where 1 is visited
 var my_maze = make_2d_int(height, width)
+
 // walls array where 1 is active 0 is torn down
-var v_walls = make_2d_wall_s(height,   width-1)
+var v_walls = make_2d_wall_s(height, width-1)
 var h_walls = make_2d_wall_s(height-1, width)
 
-func make_2d_int(h,w int) [][]int {
-	var param [][]int = make([][]int, h)
+func make_2d_int(h, w int32) [][]int32 {
+	var param [][]int32 = make([][]int32, h)
 	for i := range param {
-		param[i]=make([]int, w)
+		param[i] = make([]int32, w)
 	}
 	return param
 }
 
-func make_2d_wall_s(h, w int) [][]*wall_s {
+func make_2d_wall_s(h, w int32) [][]*wall_s {
 	var param [][]*wall_s = make([][]*wall_s, h)
 	for i := range param {
-		param[i]=make([]*wall_s, w)
+		param[i] = make([]*wall_s, w)
 	}
 	return param
 }
 
 type pos struct {
-	x, y int
+	x, y int32
 }
 
 type wall_s struct {
-	x, y, vertical, active int
+	x, y, vertical, active int32
 }
 
-func new_wall_s(x, y, vertical, active int) *wall_s {
+func new_wall_s(x, y, vertical, active int32) *wall_s {
 	return &wall_s{x: x, y: y, vertical: vertical, active: active}
 }
 
@@ -75,41 +76,41 @@ func fill_maze() {
 		}
 	}
 
-	var	x_ctr int = 0
-	var y_ctr int = 0
+	var x_ctr int32 = 0
+	var y_ctr int32 = 0
 	// all v_walls are up
 	for _, x := range v_walls {
 		for y := range x {
-			x[y] = new_wall_s(x_ctr,y_ctr,1,1)
+			x[y] = new_wall_s(x_ctr, y_ctr, 1, 1)
 			y_ctr++
 		}
-		y_ctr=0
+		y_ctr = 0
 		x_ctr++
 	}
-	x_ctr=0
-	y_ctr=0
+	x_ctr = 0
+	y_ctr = 0
 	// all h walls are up
 	for _, x := range h_walls {
 		for y := range x {
-			x[y] = new_wall_s(x_ctr,y_ctr,0,1)
+			x[y] = new_wall_s(x_ctr, y_ctr, 0, 1)
 			y_ctr++
 		}
-		y_ctr=0
+		y_ctr = 0
 		x_ctr++
 	}
 }
 
 // retruns a rand maze similiar to prims
 func rands() {
-	rand.Seed( time.Now().UTC().UnixNano())
+	rand.Seed(time.Now().UTC().UnixNano())
 	for _, x := range v_walls {
 		for y := range x {
-			x[y].active = rand.Intn(2)
+			x[y].active = int32(rand.Intn(2))
 		}
 	}
 	for _, x := range h_walls {
 		for y := range x {
-			x[y].active = rand.Intn(2)
+			x[y].active = int32(rand.Intn(2))
 		}
 	}
 }
@@ -119,42 +120,42 @@ var wall_list = list.New()
 
 // if the wall is active, add to the list of walls
 func push_wall(w *wall_s) {
-	if (w.active==1) {
+	if w.active == 1 {
 		wall_list.PushBack(w)
 	}
 }
 
 /// adds the walls surrounding the give cell
 func add_walls(cell pos) {
-	var w,h int
-	w=cell.x
-	h=cell.y
+	var w, h int32
+	w = cell.x
+	h = cell.y
 
-	var is_top = (cell.x==0)
-	var is_lft = (cell.y==0)
-	var is_bot = (cell.x==height-1)
-	var is_rgt = (cell.y==width-1)
+	var is_top = (cell.x == 0)
+	var is_lft = (cell.y == 0)
+	var is_bot = (cell.x == height-1)
+	var is_rgt = (cell.y == width-1)
 
-	if (DEBUG) {
-		print("x ", cell.x , " y ", cell.y,"\n")
-		print("t: ",is_top," b: ",is_bot," l: ",is_lft," r: ",is_rgt,"\n")
-    print(len(h_walls)," |[0] ",len(h_walls[0]),"\n")
-		print(len(v_walls)," |[0] ",len(v_walls[0]),"\n")
+	if DEBUG {
+		print("x ", cell.x, " y ", cell.y, "\n")
+		print("t: ", is_top, " b: ", is_bot, " l: ", is_lft, " r: ", is_rgt, "\n")
+		print(len(h_walls), " |[0] ", len(h_walls[0]), "\n")
+		print(len(v_walls), " |[0] ", len(v_walls[0]), "\n")
 	}
 
-	if (is_top) {
+	if is_top {
 		push_wall(h_walls[w][h])
-	}else if (is_bot) {
+	} else if is_bot {
 		push_wall(h_walls[w-1][h])
-	}else {
+	} else {
 		push_wall(h_walls[w][h])
 		push_wall(h_walls[w-1][h])
 	}
-	if (is_lft) {
+	if is_lft {
 		push_wall(v_walls[w][h])
-	}else if (is_rgt) {
+	} else if is_rgt {
 		push_wall(v_walls[w][h-1])
-	}else {
+	} else {
 		push_wall(v_walls[w][h])
 		push_wall(v_walls[w][h-1])
 	}
@@ -164,72 +165,72 @@ func prims() {
 	//	Start with a grid full of walls.
 	var cell pos
 	var wall_pos pos
-	var rand_wall int
-	var ctr int = 0
+	var rand_wall int32
+	var ctr int32 = 0
 
 	//	Pick a cell, mark it as part of the maze. Add the walls of the cell to the wall list.
-	rand.Seed( time.Now().UTC().UnixNano())
-	cell = pos{rand.Intn(height),rand.Intn(width)}
+	rand.Seed(time.Now().UTC().UnixNano())
+	cell = pos{int32(rand.Intn(int(height))), int32(rand.Intn(int(width)))}
 	my_maze[cell.x][cell.y] = 1
 	add_walls(cell)
 
 	//	While there are walls in the list:
-	for (wall_list.Len()>0) {
-		rand.Seed( time.Now().UTC().UnixNano())
-		rand_wall = rand.Intn(wall_list.Len())
-		ctr=0
+	for wall_list.Len() > 0 {
+		rand.Seed(time.Now().UTC().UnixNano())
+		rand_wall = int32(rand.Intn(wall_list.Len()))
+		ctr = 0
 		for e := wall_list.Front(); e != nil; e = e.Next() {
 			//	Pick a random wall from the list.
 			// 	//  If the cell on the opposite side isn't in the maze yet:
 			// 	//	 Make the wall a passage and mark the cell on the opposite side as part of the maze.
 			// 	//	 Add the neighboring walls of the cell to the wall list.
-			// 	//	If the cell on the opposite side already was in the maze, 
+			// 	//	If the cell on the opposite side already was in the maze,
 			//  //   remove the wall from the list.
-			if (ctr==rand_wall) {
-				if (DEBUG) {
+			if ctr == rand_wall {
+				if DEBUG {
 					print(
 						" rand_wall: x ", e.Value.(*wall_s).x,
-								   " y ", e.Value.(*wall_s).y,
-							    " vert ", e.Value.(*wall_s).vertical,
-						         " act ", e.Value.(*wall_s).active,
-						           " e ", e,
-					   " len(wall_list) ", wall_list.Len(), "\n")
+						" y ", e.Value.(*wall_s).y,
+						" vert ", e.Value.(*wall_s).vertical,
+						" act ", e.Value.(*wall_s).active,
+						" e ", e,
+						" len(wall_list) ", wall_list.Len(), "\n")
 				}
 				wall_pos = pos{e.Value.(*wall_s).x, e.Value.(*wall_s).y}
 
-				if (e.Value.(*wall_s).vertical==1) {
+				if e.Value.(*wall_s).vertical == 1 {
 					//left side
-					if (my_maze[wall_pos.x][wall_pos.y] == 0) {
+					if my_maze[wall_pos.x][wall_pos.y] == 0 {
 						add_walls(pos{wall_pos.x, wall_pos.y})
 						e.Value.(*wall_s).active = 0
 						my_maze[wall_pos.x][wall_pos.y] = 1
-					// right side
-					}else if (my_maze[wall_pos.x][wall_pos.y+1] == 0) {
-						add_walls(pos{wall_pos.x, wall_pos.y+1})
+						// right side
+					} else if my_maze[wall_pos.x][wall_pos.y+1] == 0 {
+						add_walls(pos{wall_pos.x, wall_pos.y + 1})
 						e.Value.(*wall_s).active = 0
 						my_maze[wall_pos.x][wall_pos.y+1] = 1
-					}else {
+					} else {
 						wall_list.Remove(e)
 					}
-				// horzontal
+					// horzontal
 				} else {
 					//top side
-					if (my_maze[wall_pos.x][wall_pos.y] == 0) {
+					if my_maze[wall_pos.x][wall_pos.y] == 0 {
 						add_walls(pos{wall_pos.x, wall_pos.y})
 						e.Value.(*wall_s).active = 0
 						my_maze[wall_pos.x][wall_pos.y] = 1
-					// bottom side
-					} else if (my_maze[wall_pos.x+1][wall_pos.y] == 0) {
-						add_walls(pos{wall_pos.x+1, wall_pos.y})
+						// bottom side
+					} else if my_maze[wall_pos.x+1][wall_pos.y] == 0 {
+						add_walls(pos{wall_pos.x + 1, wall_pos.y})
 						e.Value.(*wall_s).active = 0
 						my_maze[wall_pos.x+1][wall_pos.y] = 1
-					}else {
+					} else {
 						wall_list.Remove(e)
 					}
 				}
 				break
 			}
-			ctr+=1
+			ctr += 1
 		}
 	}
 }
@@ -240,106 +241,112 @@ func make_maze() {
 }
 
 // displacement from box vector origin
-var origin_disp = cord{0,-float64(width/2),-float64(height/2)}
+var origin_disp = cord{0, -float64(width / 2), -float64(height / 2)}
+
 // size of walls
-var my_size = size{1,1,.25}
+var my_size = size{1, 1, .25}
 
 type cord struct {
-	x,y,z float64
+	x, y, z float64
 }
 type size struct {
-	x,y,z float32 // redundent in a sense could cast
+	x, y, z float32 // redundent in a sense could cast
 }
 
 // creates the box vector
 func box(h float32, w float32, d float32) {
 	gl.ShadeModel(gl.SMOOTH)
 	gl.Normal3d(1.0, 1.0, 1.0)
-	red 	:= []float32{0.8, 0.1, 0.0, 1.0}
-	green := []float32{0.0, 0.8, 0.2, 1.0}
-	blue 	:= []float32{0.2, 0.2, 1.0, 1.0}
-	gl.Materialfv(gl.FRONT, gl.AMBIENT_AND_DIFFUSE, red)
+	//red := [...]float32{0.8, 0.1, 0.0, 1.0}
+	//green := [...]float32{0.0, 0.8, 0.2, 1.0}
+	//blue := [...]float32{0.2, 0.2, 1.0, 1.0}
+	var red float32 = 1.0
+	var green float32 = 1.0
+	var blue float32 = 1.0
+
+	gl.Materialfv(gl.FRONT, gl.AMBIENT_AND_DIFFUSE, &red)
+
 	var delta float32 = 0
-	// left wall  
+	// left wall
 	gl.Begin(gl.POLYGON)
-		gl.Color3f( 1.0, 0.0, 0.0 )
-		gl.Vertex3f(delta,delta,delta)
-		gl.Vertex3f(delta,h,delta)
-		gl.Vertex3f(w,h,delta)
-		gl.Vertex3f(w,delta,delta)
+	gl.Color3f(1.0, 0.0, 0.0)
+	gl.Vertex3f(delta, delta, delta)
+	gl.Vertex3f(delta, h, delta)
+	gl.Vertex3f(w, h, delta)
+	gl.Vertex3f(w, delta, delta)
 	gl.End()
 
 	//right wall
 	gl.Begin(gl.POLYGON)
-		gl.Color3f( 0.0, 0.0, 1.0 )
-		gl.Vertex3f(delta,delta,d)
-		gl.Vertex3f(delta,h,d)
-		gl.Vertex3f(w,h,d)
-		gl.Vertex3f(w,delta,d)
+	gl.Color3f(0.0, 0.0, 1.0)
+	gl.Vertex3f(delta, delta, d)
+	gl.Vertex3f(delta, h, d)
+	gl.Vertex3f(w, h, d)
+	gl.Vertex3f(w, delta, d)
 	gl.End()
 
-	gl.Materialfv(gl.FRONT, gl.AMBIENT_AND_DIFFUSE, blue)
+	gl.Materialfv(gl.FRONT, gl.AMBIENT_AND_DIFFUSE, &blue)
 	//top wall
 	gl.Begin(gl.POLYGON)
-		gl.Color3f( 0.0, 1.0, 0.0 )
-		gl.Vertex3f(delta,delta,d)
-		gl.Vertex3f(w,delta,d)
-		gl.Vertex3f(w,delta,delta)
-		gl.Vertex3f(delta,delta,delta)
+	gl.Color3f(0.0, 1.0, 0.0)
+	gl.Vertex3f(delta, delta, d)
+	gl.Vertex3f(w, delta, d)
+	gl.Vertex3f(w, delta, delta)
+	gl.Vertex3f(delta, delta, delta)
 	gl.End()
 
 	//bottom wall
 	gl.Begin(gl.POLYGON)
-		gl.Color3f( 0.5, 0.5, 0.0 )
-		gl.Vertex3f(delta,h,d)
-		gl.Vertex3f(w,h,d)
-		gl.Vertex3f(w,h,delta)
-		gl.Vertex3f(delta,h,delta)
+	gl.Color3f(0.5, 0.5, 0.0)
+	gl.Vertex3f(delta, h, d)
+	gl.Vertex3f(w, h, d)
+	gl.Vertex3f(w, h, delta)
+	gl.Vertex3f(delta, h, delta)
 	gl.End()
-	gl.Materialfv(gl.FRONT, gl.AMBIENT_AND_DIFFUSE, green)
+	gl.Materialfv(gl.FRONT, gl.AMBIENT_AND_DIFFUSE, &green)
 
-	 //forward wall
-	 gl.Begin(gl.POLYGON)
-	 	gl.Color3f( 0.0, 0.0, 1.0 )
-	 	gl.Vertex3f(delta,delta,delta)
-	 	gl.Vertex3f(delta,delta,d)
-	 	gl.Vertex3f(delta,h,d)
-	 	gl.Vertex3f(delta,h,delta)
-	 gl.End()
+	//forward wall
+	gl.Begin(gl.POLYGON)
+	gl.Color3f(0.0, 0.0, 1.0)
+	gl.Vertex3f(delta, delta, delta)
+	gl.Vertex3f(delta, delta, d)
+	gl.Vertex3f(delta, h, d)
+	gl.Vertex3f(delta, h, delta)
+	gl.End()
 
 	// //back wall
 	gl.Begin(gl.POLYGON)
-		gl.Color3f( 0.0, 0.0, 1.0 )
-		gl.Vertex3f(w,delta,delta)
-		gl.Vertex3f(w,delta,d)
-		gl.Vertex3f(w,h,d)
-		gl.Vertex3f(w,h,delta)
+	gl.Color3f(0.0, 0.0, 1.0)
+	gl.Vertex3f(w, delta, delta)
+	gl.Vertex3f(w, delta, d)
+	gl.Vertex3f(w, h, d)
+	gl.Vertex3f(w, h, delta)
 	gl.End()
 }
 
 // general draw function
 func draw() {
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT) // CARGOCULT
-	gl.PushMatrix()		// CARGOCULT
-		gl.Rotated(view_rotx, 1.0, 0.0, 0.0)
-		gl.Rotated(view_roty, 0.0, 1.0, 0.0)
-		gl.Rotated(view_rotz, 0.0, 0.0, 1.0)
-		gl.Translated(0.0, 0.0, view_z)
+	gl.PushMatrix()                                     // CARGOCULT
+	gl.Rotated(view_rotx, 1.0, 0.0, 0.0)
+	gl.Rotated(view_roty, 0.0, 1.0, 0.0)
+	gl.Rotated(view_rotz, 0.0, 0.0, 1.0)
+	gl.Translated(0.0, 0.0, view_z)
 
-		for i := range boxes {
-			gl.PushMatrix() // CARGOCULT
-			gl.CallList(boxes[i])
-			gl.PopMatrix() // CARGOCULT
-		}
+	for i := range boxes {
+		gl.PushMatrix() // CARGOCULT
+		gl.CallList(boxes[i])
+		gl.PopMatrix() // CARGOCULT
+	}
 
-	gl.PopMatrix()		 // CARGOCULT
+	gl.PopMatrix() // CARGOCULT
 
 	sdl.GL_SwapBuffers() // CARGOCULT
 }
 
 // meh....
 /* new window size or exposure */
-func reshape(width int, height int) {
+func reshape(width int32, height int32) {
 	h := float64(height) / float64(width)
 	gl.Viewport(0, 0, width, height)
 	gl.MatrixMode(gl.PROJECTION)
@@ -356,12 +363,12 @@ func reshape(width int, height int) {
 // the angles co-ordinate
 // the index of the array
 // the degree of angular rotation
-func wall(b []uint, o, offset, a_cord cord, i int, a float64, s size) []uint {
+func wall(b []uint32, o, offset, a_cord cord, i int32, a float64, s size) []uint32 {
 	b = append(b, gl.GenLists(1))
 	gl.NewList(b[i], gl.COMPILE)
-		gl.Translated(o.x+offset.x, o.y+offset.y, o.z+offset.z)
-		gl.Rotated(a, a_cord.x, a_cord.y, a_cord.z)
-		box(s.x,s.y,s.z)
+	gl.Translated(o.x+offset.x, o.y+offset.y, o.z+offset.z)
+	gl.Rotated(a, a_cord.x, a_cord.y, a_cord.z)
+	box(s.x, s.y, s.z)
 	gl.EndList()
 	return b
 }
@@ -369,42 +376,42 @@ func wall(b []uint, o, offset, a_cord cord, i int, a float64, s size) []uint {
 // void -> void
 //  builds the vector array of boxes to be drawn
 func init_walls() {
-	var ctr=0     		// ctr for box array
-	var boff cord 		// box offset
-	var x_ctr int = 0	// x ctr for walls
-	var y_ctr int = 0   // y ctr for walls
+	var ctr = 0         // ctr for box array
+	var boff cord       // box offset
+	var x_ctr int32 = 0 // x ctr for walls
+	var y_ctr int32 = 0 // y ctr for walls
 
 	for _, x := range v_walls {
 		for y := range x {
-			boff = cord{0,float64(float32(x_ctr)*(my_size.x+my_size.z)), float64(float32(my_size.y)+float32(y_ctr)*(my_size.y+my_size.z)) }
-			if (x[y].active == 1) {
-				// draw a vertical wall 
+			boff = cord{0, float64(float32(x_ctr) * (my_size.x + my_size.z)), float64(float32(my_size.y) + float32(y_ctr)*(my_size.y+my_size.z))}
+			if x[y].active == 1 {
+				// draw a vertical wall
 				// at position x_ctr y_ctt
-				boxes = wall(boxes,origin_disp, boff, cord{0,0,0},ctr,0, my_size)    // top
+				boxes = wall(boxes, origin_disp, boff, cord{0, 0, 0}, int32(ctr), 0, my_size) // top
 				ctr++
 			}
 			y_ctr++
 		}
 		x_ctr++
-		y_ctr=0
+		y_ctr = 0
 	}
 
 	for _, x := range h_walls {
 		for y := range x {
-			boff = cord{0,float64(float32(x_ctr)*(my_size.x+my_size.z)-float32(height-1)*(my_size.x+my_size.z)), float64(float32(y_ctr)*(my_size.y+my_size.z)) }
-			if (x[y].active == 1) {
+			boff = cord{0, float64(float32(x_ctr)*(my_size.x+my_size.z) - float32(height-1)*(my_size.x+my_size.z)), float64(float32(y_ctr) * (my_size.y + my_size.z))}
+			if x[y].active == 1 {
 				// draw horizontal wall
 				// at position x_ctr y_ctr
-				boxes = wall(boxes,origin_disp, boff, cord{1,0,0},ctr,90, my_size) // left
+				boxes = wall(boxes, origin_disp, boff, cord{1, 0, 0}, int32(ctr), 90, my_size) // left
 				ctr++
 			}
 			y_ctr++
 		}
 		x_ctr++
-		y_ctr=0
+		y_ctr = 0
 	}
-	x_ctr=0
-	y_ctr=0
+	x_ctr = 0
+	y_ctr = 0
 
 	// draw border
 
@@ -413,34 +420,34 @@ func init_walls() {
 // lets get things rolling
 func init_() {
 	// all for lighting effects
-	pos 	:= []float32{0.0, 0.0, 100.0, 0.0}
-	gl.Lightfv(gl.LIGHT0, gl.POSITION, pos)
+	//pos := []float32{0.0, 0.0, 100.0, 0.0}
+	var pos float32 = 1.0
+	gl.Lightfv(gl.LIGHT0, gl.POSITION, &pos)
 	gl.Enable(gl.LIGHTING)
 	gl.Enable(gl.LIGHT0)
 	// below is necessary
 	gl.Enable(gl.DEPTH_TEST)
 	gl.Enable(gl.NORMALIZE)
 
-	if (DEBUG) {
+	if DEBUG {
 		tests()
 		init_walls()
 		return
-	}else {
+	} else {
 		make_maze()
 		init_walls()
 	}
 }
-
 
 // returns if we are done or not
 func key_handler() bool {
 	var keys []uint8 = sdl.GetKeyState()
 
 	if keys[sdl.K_i] != 0 {
-		view_z +=1
+		view_z += 1
 	}
 	if keys[sdl.K_o] != 0 {
-		view_z -=1
+		view_z -= 1
 	}
 	if keys[sdl.K_ESCAPE] != 0 {
 		return true
@@ -471,16 +478,16 @@ func key_handler() bool {
 		gl.Translatef(0.0, 0.0, -1)
 	}
 	if keys[sdl.K_a] != 0 {
-		gl.Translatef(-1,0, 0)
+		gl.Translatef(-1, 0, 0)
 	}
 	if keys[sdl.K_d] != 0 {
-		gl.Translatef(1,0, 0)
+		gl.Translatef(1, 0, 0)
 	}
 	if keys[sdl.K_q] != 0 {
-		gl.Translatef(0,1, 0)
+		gl.Translatef(0, 1, 0)
 	}
 	if keys[sdl.K_e] != 0 {
-		gl.Translatef(0,-1, 0)
+		gl.Translatef(0, -1, 0)
 	}
 	return false
 }
@@ -496,11 +503,12 @@ func main() {
 		sdl.Quit()
 		panic("Couldn't set 300x300 GL video mode: " + sdl.GetError() + "\n")
 	}
-	if gl.Init() != 0 {
-		panic("gl error")
-	}
+	gl.Init()
+	//if gl.Init() != nil {
+	//panic("gl error")
+	//}
 	init_()
-	reshape(int(screen.W), int(screen.H))
+	reshape(int32(screen.W), int32(screen.H))
 	done = false
 	for !done {
 		for e := sdl.PollEvent(); e != nil; e = sdl.PollEvent() {
@@ -510,7 +518,7 @@ func main() {
 				screen = sdl.SetVideoMode(int(re.W), int(re.H), 16,
 					sdl.OPENGL|sdl.RESIZABLE)
 				if screen != nil {
-					reshape(int(screen.W), int(screen.H))
+					reshape(int32(screen.W), int32(screen.H))
 				} else {
 					panic("we couldn't set the new video mode??")
 				}
@@ -529,13 +537,13 @@ func main() {
 
 func tests() {
 	fill_maze()
-	var w int = width -1
-	var h int = height -1
+	var w int32 = width - 1
+	var h int32 = height - 1
 
-	var tl = pos{0,0}
-	var bl = pos{h,0}
-	var tr = pos{0,w}
-	var br = pos{h,w}
+	var tl = pos{0, 0}
+	var bl = pos{h, 0}
+	var tr = pos{0, w}
+	var br = pos{h, w}
 
 	var test_arr [4]pos
 	test_arr[0] = tl
@@ -543,14 +551,14 @@ func tests() {
 	test_arr[2] = tr
 	test_arr[3] = br
 
-	h_walls[1][1].active = 0;
-	h_walls[2][2].active = 0;
-	h_walls[3][3].active = 0;
+	h_walls[1][1].active = 0
+	h_walls[2][2].active = 0
+	h_walls[3][3].active = 0
 
-	var top_left = pos{0,0}
-	var bot_rght = pos{h,w}
-	var top_rght = pos{0,w}
-	var bot_left = pos{h,0}
+	var top_left = pos{0, 0}
+	var bot_rght = pos{h, w}
+	var top_rght = pos{0, w}
+	var bot_left = pos{h, 0}
 	print("top left test \n")
 	add_walls(top_left)
 	// test wall_list length
